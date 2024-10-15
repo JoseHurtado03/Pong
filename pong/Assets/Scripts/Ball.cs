ba65296f8499ad16c97660b1007f628b84d09996
JoseHurtado03 <jose.hurtado@correo.unimet.edu.ve>
2024-10-15 09:17:56 -0400

    :fire: Mejoras

--- pong/Assets/Scripts/Ball.cs
+++ pong/Assets/Scripts/Ball.cs
@@ -9,7 +9,7 @@ public float vel;
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(vel, 0);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(vel, vel);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -17,13 +17,14 @@ public float vel;
         {
             GameManager.Instance.PointP1();
             transform.position = Vector2.zero;
+            vel = 4;
         }
 
         if (collision.gameObject.CompareTag("ScoreP2"))
         {
             GameManager.Instance.PointP2();
             transform.position = Vector2.zero;
+            vel = 4;
         }
     }
-
 }
--- pong/Assets/Scripts/Player.cs
+++ pong/Assets/Scripts/Player.cs
@@ -8,18 +8,29 @@ public class Player : MonoBehaviour
     public KeyCode down;
     public float velY;
 
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     // Update is called once per frame
     void Update()
     {
+        // Reiniciar la velocidad vertical a 0 cada frame
+        rb.velocity = new Vector2(0, 0);
+
+        // Mover la raqueta hacia arriba si se presiona la tecla W/Flecha arriba
         if (Input.GetKey(up))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, velY);
+            rb.velocity = new Vector2(0, velY);
         }
 
+        // Mover la raqueta hacia abajo si se presiona la tecla S/Flecha abajo
         if (Input.GetKey(down))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, -velY);
+            rb.velocity = new Vector2(0, -velY);
         }
     }
 }
-    
